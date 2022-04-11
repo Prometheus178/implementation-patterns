de7db19ec7d7887c1810612310a427837d0843a0
sergey <sergeipopof178@gmail.com>
2022-04-11 14:14:32 +0300

    bridge

---
+++ src/structure/bridge/remotes/AdvancedRemote.java
@@ -0,0 +1,16 @@
+package structure.bridge.remotes;
+
+import structure.bridge.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+
+}
