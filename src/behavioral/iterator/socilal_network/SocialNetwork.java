5503e69244461f70c7f43956f45faf8e4faeb429
sergey <sergeipopof178@gmail.com>
2022-04-14 17:43:27 +0300

    iterator

---
+++ src/behavioral/iterator/socilal_network/SocialNetwork.java
@@ -0,0 +1,10 @@
+package behavioral.iterator.socilal_network;
+
+import behavioral.iterator.iterators.ProfileIterator;
+
+public interface SocialNetwork {
+
+    ProfileIterator createFriendsIterator(String profileEmail);
+
+    ProfileIterator createCoworkersIterator(String profileEmail);
+}
